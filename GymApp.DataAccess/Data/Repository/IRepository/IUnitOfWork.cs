cafc8ee6f64065b51eda715138a33f1a9ede556a
Natalia <natgru1998@gmail.com>
2020-11-30 20:07:31 +0100

    add and implement UnitOfWork and IUnitOfWork

--- GymApp.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
+++ GymApp.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -4,7 +4,10 @@ using System.Text;
 
 namespace GymApp.DataAccess.Data.Repository.IRepository
 {
-    class IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
+        ICategoryRepository Category { get; }
+
+        void Save();
     }
 }
--- GymApp.DataAccess/Data/Repository/UnitOfWork.cs
+++ GymApp.DataAccess/Data/Repository/UnitOfWork.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using GymApp.DataAccess.Data.Repository.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GymApp.DataAccess.Data.Repository
 {
-    class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
     }
+    
 }
