8bb22b55870b9034b625e49220673279095fc119
Natalia <natgru1998@gmail.com>
2020-11-30 19:57:43 +0100

    add and implement CategoryRepository and ICategoryRepository

--- GymApp.DataAccess/ApplicationDbContext.cs
+++ GymApp.DataAccess/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GymApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace GymApp.DataAccess
             : base(options)
         {
         }
+
+        public DbSet<Category> Category { get; set; }
     }
 }
--- GymApp.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
+++ GymApp.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using GymApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GymApp.DataAccess.Data.Repository.IRepository
 {
-    class ICategoryRepository
+    public interface ICategoryRepository : IRepository<Category>
     {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+
+        void Update(Category category);
     }
 }
